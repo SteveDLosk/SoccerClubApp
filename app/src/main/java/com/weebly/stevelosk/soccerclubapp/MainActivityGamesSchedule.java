5636ee0e593176383a56188e6c883a6997559043
Steve Losk <Stevedlosk@gmail.com>
2017-05-20 13:28:02 -0700

    Initial commit

---
+++ app/src/main/java/com/weebly/stevelosk/soccerclubapp/MainActivityGamesSchedule.java
@@ -0,0 +1,13 @@
+package com.weebly.stevelosk.soccerclubapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityGamesSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_games_schedule);
+    }
+}
